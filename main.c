580b694cbc7529e07670bcfd4967c2df54616baa
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-12-25 05:10:00 +0100

    feat: Implement simple shell

--- main.c
+++ main.c
@@ -1,56 +1,45 @@
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
 #include "shell.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
+* main - entry point
+* @ac: arg count
+* @av: arg vector
+*
+* Return: 0 on success, 1 on error
+*/
+
+int main(int ac, char **av)
 {
-	char *line = NULL, **u_tokns = NULL;
-	size_t line_size = 0;
-	ssize_t line_len = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	while (line_len >= 0)
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
 	{
-		int w_len = 0;
-
-		signal(SIGINT, signal_handler);
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
-		line_len = getline(&line, &line_size, stdin);
-		if (line_len == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-		w_len = count_input(line);
-		if (line[0] != '\n' && w_len > 0)
-		{
-			int execFlag = 0;
-
-			u_tokns = tokenize(line, " \t", w_len);
-			execFlag = execBuiltInCommands(u_tokns, line);
-			if (!execFlag)
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
 			{
-				u_tokns[0] = find(u_tokns[0]);
-				if (u_tokns[0] && access(u_tokns[0], X_OK) == 0)
-					exec(u_tokns[0], u_tokns);
-				else
-					perror("./hsh");
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
 			}
-			free_tokens(u_tokns);
+			return (EXIT_FAILURE);
 		}
+		info->readfd = fd;
 	}
-	free(line);
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
